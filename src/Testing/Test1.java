e4361c5e7917368d0030348603b7034fdd80866e
Nav <114872151+Navjot1212@users.noreply.github.com>
2023-04-27 12:57:13 -0400

    c11 (#10)

--- src/Testing/Test1.java
+++ src/Testing/Test1.java
@@ -3,6 +3,6 @@ package Testing;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("I am making changes in master");
+		System.out.println("Conflict resolved");
 	}
 }
